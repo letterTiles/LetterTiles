341cc7400695c3d4a06bd9341b169872afa30445
Austin Stoker <austin@austinstoker.com>
2021-08-05 02:50:11 -0600

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,8 +45,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8d5a56337a75c65596b9f6923dafa776",
-"/": "8d5a56337a75c65596b9f6923dafa776",
+"index.html": "c28cde86f62bd66513d4d2ff353a29cf",
+"/": "c28cde86f62bd66513d4d2ff353a29cf",
 "main.dart.js": "43d4a24bc78eb5c029bbd275b8717830",
 "manifest.json": "667b57d617695152bc8cb1cef352eb2b",
 "version.json": "579dcdc0b4abd93065110cecd12a71b5"
