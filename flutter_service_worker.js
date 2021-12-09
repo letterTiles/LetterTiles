4af9283120d959ffe1531cdca15e133a30c99092
Austin Stoker <austin@austinstoker.com>
2021-12-08 23:38:57 -0700

    Pushing Demo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,10 +29,14 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "bcc1efbd2e9c82a3e536b6efee1595ff",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "icons/android-icon-144x144.png": "dbc40df879e15ab362924f1aafb02d45",
-"index.html": "3b932381ab294b74a3113b61a6f0d0eb",
+"index.html": "d87e14812b4854ac11dbf94ba54d6ece",
  
-"main.dart.js": "d0d2a1e79ba8990870b5dfad2f1a8b69",
+"main.dart.js": "256c12696575b25517529a14d5525e56",
 "manifest.json": "2ebf79023cee92a5c824a88178cbe9a1",
 "version.json": "7ac24d16e38d491bec4e62c2ed9ce749"
 };
