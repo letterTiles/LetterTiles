3bcb4093c9dc5e8518df6cd5f44a3d35dc58dd98
Austin Stoker <austin@austinstoker.com>
2021-12-22 09:10:22 -0700

    Pushing Demo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "icons/android-icon-144x144.png": "dbc40df879e15ab362924f1aafb02d45",
-"index.html": "7fa4a688cbb01808d64778c3a9b0cfc9",
+"index.html": "a8e8f72bddc85be989205e443c425aa7",
  
-"main.dart.js": "d6ef05c21502dbd1a7445eca87b666ee",
+"main.dart.js": "b025fbd4c28c07a5b88ff0ae5e20f44d",
 "manifest.json": "2ebf79023cee92a5c824a88178cbe9a1",
 "version.json": "7ac24d16e38d491bec4e62c2ed9ce749"
 };
